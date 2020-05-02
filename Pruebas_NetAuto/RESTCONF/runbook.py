9a380a340968d23526239dd72599fc27048d5604
Fernando Tapia <fernando.tapia.vaca@gmail.com>
2020-05-01 23:31:31 -0400

    Black format

--- Pruebas_NetAuto/RESTCONF/runbook.py
+++ Pruebas_NetAuto/RESTCONF/runbook.py
@@ -5,43 +5,45 @@ from nornir import InitNornir
 from nornir.plugins.tasks.apis import http_method
 from nornir.plugins.functions.text import print_result
 
+
 def manage_rt(task, vrf_target, headers):
     # host-specific URL
     base_url = f"https://{task.host.hostname}/restconf"
 
     # Task 1: Use http_method to get the vrf config via HTTP GET
     task.run(
-        task = http_method,
-        name = "Get VRF config via HTTP GET",
-        method = "get",
-        url = base_url + vrf_target,
-        auth = ("admin", "admin"),
-        headers = headers['get'],
-        verify = False,
+        task=http_method,
+        name="Get VRF config via HTTP GET",
+        method="get",
+        url=base_url + vrf_target,
+        auth=("admin", "admin"),
+        headers=headers["get"],
+        verify=False,
     )
 
     # Task 2: User http_method to update the VRF config via HTTP PUT
     task.run(
-        task = http_method,
-        name = "Update VRF config via HTTP PUT",
-        method = "put",
-        url = base_url + vrf_target,
-        auth = ("admin", "admin"),
-        headers = headers['put_post'],
-        verify = False,
-        json = task.host['body'],
+        task=http_method,
+        name="Update VRF config via HTTP PUT",
+        method="put",
+        url=base_url + vrf_target,
+        auth=("admin", "admin"),
+        headers=headers["put_post"],
+        verify=False,
+        json=task.host["body"],
     )
 
     # Task 3: Save the config using HTTP POST
     task.run(
-        task = http_method,
-        name = "Save VRF config via HTTP POST",
-        method = "post",
-        url = base_url + "operations/cisco-ia:save-config",
-        auth = ("admin", "admin"),
-        headers = headers['put_post'],
-        verify = False,
+        task=http_method,
+        name="Save VRF config via HTTP POST",
+        method="post",
+        url=base_url + "operations/cisco-ia:save-config",
+        auth=("admin", "admin"),
+        headers=headers["put_post"],
+        verify=False,
     )
+
 
 def main():
 
@@ -59,13 +61,14 @@ def main():
     }
     nornir = InitNornir()
     result = nornir.run(
-        task = manage_rt,
-        name = "Manage Devices via RESTCONF",
-        vrf_target = vrf_target,
-        headers = headers,
+        task=manage_rt,
+        name="Manage Devices via RESTCONF",
+        vrf_target=vrf_target,
+        headers=headers,
     )
 
     print_result(result)
+
 
 if __name__ == "__main__":
     main()
